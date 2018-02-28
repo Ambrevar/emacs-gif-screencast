f7b5df1f1d4d291a4c745eee5b94112ca7793df1
Takaaki ISHIKAWA <takaxp@ieee.org>
2018-03-01 02:38:49 +0900

    Ensure autoremoving of screenshots.

--- gif-screencast.el
+++ gif-screencast.el
@@ -191,21 +191,17 @@ If you are a macOS user, \"ppm\" should be specified."
                       ;; Delays must come after the file arguments.
                       (apply 'nconc delays)
                       (list output))))
-      (if gif-screencast-want-optimized
-          (set-process-sentinel p (lambda (process event)
-                                    (if (and (eq (process-status process) 'exit)
-                                             (= (process-exit-status process) 0))
-                                        (setq p (gif-screencast-optimize output))
-                                      (gif-screencast-print-status process event))))
-        (set-process-sentinel p 'gif-screencast-print-status))
-      (if gif-screencast-autoremove-screenshots
-          (set-process-sentinel p (lambda (process event)
-                                    (if (and (eq (process-status process) 'exit)
-                                             (= (process-exit-status process) 0))
-                                        (dolist (f gif-screencast--frames)
-                                          (delete-file (cdr f))))
-                                    (gif-screencast-print-status process event)))
-        (set-process-sentinel p 'gif-screencast-print-status)))))
+      (set-process-sentinel p (lambda (process event)
+                                (gif-screencast-print-status process event)
+                                (when (and gif-screencast-want-optimized
+                                           (eq (process-status process) 'exit)
+                                           (= (process-exit-status process) 0))
+                                  (gif-screencast-optimize output))
+                                (when (and gif-screencast-autoremove-screenshots
+                                           (eq (process-status process) 'exit)
+                                           (= (process-exit-status process) 0))
+                                  (dolist (f gif-screencast--frames)
+                                    (delete-file (cdr f)))))))))
 
 (defun gif-screencast--cropping-region ()
   "Return the cropping region of the captured image."
